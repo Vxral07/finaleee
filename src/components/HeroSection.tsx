0ff918667f3a5b2b54f2586f7930d73dbedb3cb2
Allen Pereira <allenpereira@Allens-MacBook-Air.local>
2024-12-13 20:06:14 +0530

    sdads

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -12,8 +12,8 @@ export const HeroSection: React.FC<HeroSectionProps> = ({
       <div className="flex gap-10 items-center max-md:flex-col">
         {/* Left Section */}
         <div className="flex flex-col w-[44%] ml-12 max-md:ml-0 max-md:w-full">
-          <div className="self-stretch text-5xl ml-5 font-medium tracking-tighter leading-tight text-white max-md:mt-10  max-md:max-w-full max-md:text-4xl">
-            <span className="text-cyan-500">{highlightedText} </span>
+          <div className="self-stretch text-5xl ml-5 font-medium tracking-tighter leading-tight text-cyan-500 max-md:mt-10  max-md:max-w-full max-md:text-4xl">
+            <span className="text-white">{highlightedText} </span>
             {title}
           </div>
           {/* Description Section */}
